ce0dd808b9918fc20dec4b0352c39bc501657423
Arian <arianchy250@gmail.com>
2026-09-03 19:39:28 +0600

    Javascript operator

---
+++ index.js
@@ -0,0 +1,42 @@
+let material = {
+  name: "Gold",
+  karat: "22karat",
+  value: 130000,
+  possibility: "2%",
+};
+
+const student1 = {
+  name: "Arisfa",
+  age: 4,
+  gender: "female",
+  school: "Chittagong school",
+};
+
+let prime_minister = {
+  Name: "Sheikh hasina",
+  Party: "Awami League",
+  Age: 84,
+  Rating: 7.2,
+};
+
+let second_prime_minister = {
+  Name: "Zarek Tia",
+  Party: " Bangladesh National Party(BNP)",
+  Age: 57,
+  Rating: 4.6,
+  knowledge: "Beginner",
+};
+
+second_prime_minister.Name = "Tarek Zia";
+second_prime_minister["Age"] = 60;
+second_prime_minister["Rating0"] = 3.5;
+
+second_prime_minister["knowledge"] = "Intermidiate";
+
+const user = {
+  name: "Arian",
+  email: "arian@gmail.com",
+  password: "11111",
+};
+
+user["password"] = "1234";
